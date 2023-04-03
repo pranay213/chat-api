d359b07cb462a3903107e0e2ef98087355d88393
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-04-03 14:48:42 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 const port = 3000;
-app.use(json());
+app.use(express());
 app.get("/", (req, res) => {
   //   res.send("Hello World!");
   res.send({ Status: "Server is Running" });
